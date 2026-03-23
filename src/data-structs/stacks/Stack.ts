c86db4a8b022249cb96feb135f004de2e57631f7
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-23 17:17:59 +0200

    feat: Create `Stack` class

---
+++ src/data-structs/stacks/Stack.ts
@@ -0,0 +1,37 @@
+import Collection from "../collection/Collection";
+
+/**
+ * A last in first out (LIFO)
+ */
+export default class Stack<T> extends Collection<T> {
+  private items: T[];
+
+  constructor() {
+    super();
+    this.items = [];
+  }
+
+  get length() {
+    return this.items.length;
+  }
+
+  override remove(): T | undefined {
+    return this.items.pop();
+  }
+
+  override add(item: T): void {
+    this.items.push(item);
+  }
+
+  override clear(): void {
+    this.items = [];
+  }
+
+  override peek(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+
+  override toString(): string {
+    return ""; // TODO -> Implement toString()
+  }
+}
