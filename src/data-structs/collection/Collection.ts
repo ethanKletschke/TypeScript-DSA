0376a63545273928168b99c377193405e7fde03a
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-23 13:15:05 +0200

    docs: Add TSDoc comment to `Collection`

--- src/data-structs/collection/Collection.ts
+++ src/data-structs/collection/Collection.ts
@@ -1,5 +1,9 @@
 /**
+ * An abstract class for a collection
  *
+ * @remarks
+ * This abstract class defines the simple structure
+ * for a collection, like a queue or a stack.
  */
 export default abstract class Collection<T> {
   abstract get length(): number;
