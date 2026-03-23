6922358b96ea5ac89f281e1b63c1fbba84b60706
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-23 13:11:42 +0200

    fix(src): Allow `undefined` in `peek()` definition

--- src/data-structs/collection/Collection.ts
+++ src/data-structs/collection/Collection.ts
@@ -5,7 +5,7 @@ export default abstract class Collection<T> {
   abstract length: number;
   abstract pop(): T;
   abstract clear(): void;
-  abstract peek(): T;
+  abstract peek(): T | undefined;
 
   isEmpty(): boolean {
     return this.length === 0;
