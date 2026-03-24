6a39f3b723aefc054043258b69dd58292d05a12a
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-24 08:22:48 +0200

    wip(feat): Create `Queue` class

---
+++ src/data-structs/Queue.ts
@@ -0,0 +1,10 @@
+import Collection from "./Collection";
+
+/**
+ * A FIFO (First In First Out) data structure
+ * where items are added to the back and removed from
+ * the front.
+ */
+export default class Queue<T> extends Collection<T> {
+
+}
