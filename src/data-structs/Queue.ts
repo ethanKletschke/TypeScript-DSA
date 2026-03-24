76abe1c7a63e42cb039d24e5e7c08d348f1b39e9
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-24 08:27:20 +0200

    feat: Add basic functionality to `Queue`

--- src/data-structs/Queue.ts
+++ src/data-structs/Queue.ts
@@ -6,5 +6,19 @@ import Collection from "./Collection";
  * the front.
  */
 export default class Queue<T> extends Collection<T> {
+  private items: T[];
+
+  constructor() {
+    super();
+    this.items = [];
+  }
+
+  override add(item: T): void {
+    this.items.push(item);
+  }
+
+  override remove(): T | undefined {
+    return this.items.shift(); // Dequeue from front
+  }
 
 }
