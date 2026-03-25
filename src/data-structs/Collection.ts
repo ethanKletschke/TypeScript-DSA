47c03de44e65384d5aa39dcf6df8f2e2fe14f06e
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-25 06:31:52 +0200

    feat: Define `Collection.addItems()` abstract method

--- src/data-structs/Collection.ts
+++ src/data-structs/Collection.ts
@@ -6,7 +6,7 @@ export default abstract class Collection<T> {
   abstract get length(): number;
   abstract remove(): T | undefined;
   abstract add(item: T): void;
-  // TODO -> multiple add() with spread operator
+  abstract addItems(...items: T[]): void;
   abstract clear(): void;
   abstract peek(): T | undefined;
   abstract toString(): string;
