f2e209bd6fe293c9605ffa674c82bef63607b3b4
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-25 05:45:05 +0200

    chore(deps): Implement ESLint configs

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+
+    }
+  },
+  {
+    files: ["**/*.spec.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
+    }
+  }
+];
