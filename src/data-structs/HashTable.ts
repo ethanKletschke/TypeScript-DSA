f125bc97e27a8cf8fc71db9e3f009b74ed2865c8
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-30 17:06:58 +0200

    feat: Define `HashTable` hashing function

--- src/data-structs/HashTable.ts
+++ src/data-structs/HashTable.ts
@@ -12,4 +12,23 @@ export class HashTable<V> {
   get size() {
     return this.items.length;
   }
+
+  /**
+   * Defines which key to put a value under
+   *
+   * @remarks
+   * Implemented using the DJB2 algorithm
+   */
+  public hashFunc(key: string): number {
+    // Define a seed for the hash
+    let hash = 5381;
+
+    // For each character in the key
+    for (let i = 0; i < key.length; i++) {
+      // Add to the hash
+      hash = (hash * 33) ^ key.charCodeAt(i);
+    }
+
+    return Math.abs(hash) % this.size;
+  }
 }
