2905752877b83b0f2fe9f163860e3277dc54b19b
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-30 08:43:26 +0200

    fix: Tweak `partition()` to remove bugs and quirks

--- src/algorithms/sort/quicksort.ts
+++ src/algorithms/sort/quicksort.ts
@@ -1,17 +1,20 @@
-function partition<T>(arr: T[], low: number, high: number): number {
-  const newArr = [...arr];
+function partition<T extends string | number | bigint>(
+  arr: T[],
+  low: number,
+  high: number
+): number {
   const pivot: T = arr[high];
 
   let i = low - 1;
 
   for (let j = low; j < high; j++) {
-    if (newArr[j] <= pivot) {
+    if (arr[j] <= pivot) {
       i += 1;
-      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
   }
 
-  [newArr[i + 1], newArr[high]] = [newArr[high], arr[i + 1]];
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
 
   return i + 1;
 }
