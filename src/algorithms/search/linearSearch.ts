16da622fc3098ed1b1169d06fa05b3db0d384ac3
Ethan Kletschke <bbruhingson@gmail.com>
2026-04-01 07:28:21 +0200

    wip: Create function skeleton for `linearSearch()`

---
+++ src/algorithms/search/linearSearch.ts
@@ -0,0 +1,13 @@
+/**
+ * An implementation of Linear Search that searches a generic
+ * array for a specified value.
+ *
+ * @param arr - The array to search.
+ * @param val - The value to search for.
+ * @returns The index of `val` in `arr`.
+ *
+ * @experimental
+ */
+export function linearSearch<T>(arr: T[], val: T): number {
+  return -1;
+}
