b2abe1100210ef92463608599b4368ca1d5b1f09
Ethan Kletschke <bbruhingson@gmail.com>
2026-04-03 06:31:24 +0200

    feat: Add constructor to `TreeNode` class

--- src/data-structs/trees/TreeNode.ts
+++ src/data-structs/trees/TreeNode.ts
@@ -2,4 +2,10 @@ export class TreeNode<T> {
   private data: T | undefined;
   private left: TreeNode<T>;
   private right: TreeNode<T>;
+
+  constructor(data?: T) {
+    this.data = data;
+    this.left = new TreeNode<T>();
+    this.right = new TreeNode<T>();
+  }
 }
