08118075feb65e5577e67411726f48f870c346bd
Ethan Kletschke <bbruhingson@gmail.com>
2026-04-03 08:24:39 +0200

    feat: Fix recursive constructor and implement properties

--- src/data-structs/trees/TreeNode.ts
+++ src/data-structs/trees/TreeNode.ts
@@ -1,11 +1,54 @@
+/**
+ * A node for a tree data structure.
+ *
+ * @typeParam T - the data type of the node's data
+ * @beta
+ */
 export class TreeNode<T> {
-  private data: T | undefined;
-  private left: TreeNode<T>;
-  private right: TreeNode<T>;
+  private _data: T | undefined;
+  private _left: TreeNode<T> | null = null;
+  private _right: TreeNode<T> | null = null;
 
+
+  /**
+   * The `constructor` method creates an instance of the `TreeNode` class.
+   *
+   * @param {?T} [data] - The data for the current node
+   */
   constructor(data?: T) {
-    this.data = data;
-    this.left = new TreeNode<T>();
-    this.right = new TreeNode<T>();
+    this._data = data;
+  }
+
+  /**
+   * The node to the left of the current node.
+   */
+  get leftNode(): TreeNode<T> | null {
+    return this._left;
+  }
+
+  set leftNode(node: TreeNode<T>) {
+    this._left = node;
+  }
+
+  /**
+   * The node to the right of the current node.
+   */
+  get rightNode(): TreeNode<T> | null {
+    return this._right;
+  }
+
+  set rightNode(node: TreeNode<T>) {
+    this._right = node;
+  }
+
+  /**
+   * The data for the current tree node.
+   */
+  get data(): T | undefined {
+    return this._data;
+  }
+
+  set data(data: T) {
+    this._data = data;
   }
 }
