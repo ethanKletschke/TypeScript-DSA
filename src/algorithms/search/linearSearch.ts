64fff3d8ff8f39f9393826ff6aa2732a5a8a422f
Ethan Kletschke <bbruhingson@gmail.com>
2026-04-05 11:05:45 +0200

    docs: Add documentation to `linearSearch()` algo

--- src/algorithms/search/linearSearch.ts
+++ src/algorithms/search/linearSearch.ts
@@ -3,14 +3,20 @@
  * array for a specified value.
  *
  * @param arr - The array to search.
- * @param val - The value to search for.
+ * @param val - The value to search for in `arr`.
  * @returns The index of `val` in `arr`, or `-1` if not found
+ *
+ * @author Ethan Kletschke
  */
 export function linearSearch<T>(arr: T[], val: T): number {
+  // Declare the found index as -1
   let index = -1;
 
+  // Loop through the array's indexes
   for (let i = 0; i < arr.length; i++) {
+    // If the item to search for is found
     if (arr[i] === val) {
+      // Change the found index to that index
       index = i;
     }
   }
